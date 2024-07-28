5ca2087dfa1ac0e3b0fb92be7a324fdbaac786c1
agnosto <agnostoshi@protonmail.com>
2024-07-28 16:07:25 -0700

    update: updater command for new workflow

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 )
 
 var ffmpegAvailable bool
-const version = "0.0.9"
+const version = "v0.1.0"
 
 func main() {
     if len(os.Args) > 1 && os.Args[1] == "update" {
--- updater/updater.go
+++ updater/updater.go
@@ -1,13 +1,16 @@
 package updater
 
 import (
+    "archive/tar"
+    "compress/gzip"
     "encoding/json"
     "fmt"
     "io"
     "net/http"
     "os"
-    //"path/filepath"
+    "path/filepath"
     "runtime"
+    "strings"
 )
 
 const (
@@ -26,6 +29,10 @@ func CheckForUpdate(currentVersion string) error {
     release, err := getLatestRelease()
     if err != nil {
         return fmt.Errorf("failed to get latest release: %w", err)
+    }
+
+    if !strings.HasPrefix(currentVersion, "v") {
+        currentVersion = "v" + currentVersion
     }
 
     if release.TagName == currentVersion {
@@ -58,11 +65,8 @@ func getLatestRelease() (*GithubRelease, error) {
 }
 
 func updateBinary(release *GithubRelease) error {
-    assetName := fmt.Sprintf("fansly-scraper-%s-%s", runtime.GOOS, runtime.GOARCH)
-    if runtime.GOOS == "windows" {
-        assetName += ".exe"
-    }
-
+    assetName := fmt.Sprintf("fansly-scraper_%s_%s_%s.tar.gz", strings.TrimPrefix(release.TagName, "v"), runtime.GOOS, runtime.GOARCH)
+    
     var downloadURL string
     for _, asset := range release.Assets {
         if asset.Name == assetName {
@@ -70,45 +74,74 @@ func updateBinary(release *GithubRelease) error {
             break
         }
     }
-
+    
     if downloadURL == "" {
         return fmt.Errorf("no suitable binary found for your system")
     }
-
+    
     fmt.Println("Downloading new version...")
     resp, err := http.Get(downloadURL)
     if err != nil {
         return err
     }
     defer resp.Body.Close()
-
-    execPath, err := os.Executable()
+    
+    tempDir, err := os.MkdirTemp("", "fansly-scraper-update")
     if err != nil {
         return err
     }
-
-    tempPath := execPath + ".new"
-    out, err := os.Create(tempPath)
+    defer os.RemoveAll(tempDir)
+    
+    gzr, err := gzip.NewReader(resp.Body)
     if err != nil {
         return err
     }
-    defer out.Close()
-
-    _, err = io.Copy(out, resp.Body)
-    if err != nil {
-        return err
+    defer gzr.Close()
+    
+    tr := tar.NewReader(gzr)
+    
+    for {
+        header, err := tr.Next()
+        if err == io.EOF {
+            break
+        }
+        if err != nil {
+            return err
+        }
+        
+        if header.Typeflag == tar.TypeReg {
+            outPath := filepath.Join(tempDir, header.Name)
+            outFile, err := os.Create(outPath)
+            if err != nil {
+                return err
+            }
+            if _, err := io.Copy(outFile, tr); err != nil {
+                outFile.Close()
+                return err
+            }
+            outFile.Close()
+            
+            if strings.HasPrefix(header.Name, "fansly-scraper") {
+                execPath, err := os.Executable()
+                if err != nil {
+                    return err
+                }
+                
+                err = os.Chmod(outPath, 0755)
+                if err != nil {
+                    return err
+                }
+                
+                err = os.Rename(outPath, execPath)
+                if err != nil {
+                    return err
+                }
+                
+                fmt.Println("Update successful. Please restart the application.")
+                return nil
+            }
+        }
     }
-
-    err = os.Chmod(tempPath, 0755)
-    if err != nil {
-        return err
-    }
-
-    err = os.Rename(tempPath, execPath)
-    if err != nil {
-        return err
-    }
-
-    fmt.Println("Update successful. Please restart the application.")
-    return nil
+    
+    return fmt.Errorf("binary not found in the archive")
 }
